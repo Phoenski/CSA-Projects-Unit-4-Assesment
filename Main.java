5d7cf3fce479a8a58bd4ae2da419c3aad107421f
Phoenski <69543226+Phoenski@users.noreply.github.com>
2022-01-09 23:17:18 -0500

    Update

--- Main.java
+++ Main.java
@@ -1,27 +1,136 @@
+import java.util.*;
+
 class Main
 {
     public static void main(String [] args)
     {
         //
-        String n;
-        SixDigFuncClass InitialSixDigString = new SixDigFuncClass();
-        for (int i = 0; i < 1000000; i++)
+        SixDigFuncClass InitialSixDigString = new SixDigFuncClass("000000");
+        for (int i = 0; i < 10; i++)
         {
             //
             System.out.println(InitialSixDigString);
+            InitialSixDigString.toString();
+            InitialSixDigString.getLeadingZeros();
+            InitialSixDigString.truncateLeadingZeros();
+            InitialSixDigString.recordStringLengthBeforeIncrease();
+            InitialSixDigString.IntSixDigStringPlusPlus();
+            InitialSixDigString.toString();
+            InitialSixDigString.checkHighestPlaceValueChanged();
         }
     }
 }
+
 class SixDigFuncClass
 {
     //
-    public String SixDigFunc(String SixDigString)
+    private String DynamicSixDigString;
+    private String LeadingZeros;
+    private int PosOfFirstZero;
+    private int IntSixDigString;
+    private int StringLengthBeforeIncrease;
+    private int StringLengthAfterIncrease;
+    private boolean HighestPlaceValueChanged;
+    public SixDigFuncClass(String StaticSixDigString)
     {
         //
-        SixDigString = "000000";
-        String ZerosToAddBack;
-        int SixDigInt;
-        if (SixDigString.charAt(0) == 0) // 1 (0)00000
+        DynamicSixDigString = StaticSixDigString;
+    }
+
+    @Override
+    public String toString()
+    {
+        return DynamicSixDigString;
+    }
+
+    public int PosOfFirstZero(int PosOfFirstZero)
+    {
+        PosOfFirstZero = DynamicSixDigString.indexOf("0");
+        return PosOfFirstZero;
+    }
+
+    public String getLeadingZeros()
+    {
+        if (PosOfFirstZero == 0)
+        {
+            LeadingZeros = "0";
+        }
+        else if (PosOfFirstZero == 1)
+        {
+            LeadingZeros = "00";
+        }
+        else if (PosOfFirstZero == 2)
+        {
+            LeadingZeros = "000"; 
+        }
+        else if (PosOfFirstZero == 3)
+        {
+            LeadingZeros = "0000";
+        }
+        else if (PosOfFirstZero == 4)
+        {
+            LeadingZeros = "00000";
+        }
+        else if (PosOfFirstZero == 5)
+        {
+            LeadingZeros = "00000";
+        }
+        else
+        {
+            return "Error";
+        }
+        return LeadingZeros;
+    }
+
+    public String truncateLeadingZeros()
+    {
+        //
+        DynamicSixDigString = DynamicSixDigString.substring(PosOfFirstZero);
+        return DynamicSixDigString;
+    }
+
+    public int recordStringLengthBeforeIncrease()
+    {
+        StringLengthBeforeIncrease = DynamicSixDigString.length();
+        return StringLengthBeforeIncrease;
+    }
+
+    public int doStringToInt()
+    {
+        IntSixDigString = Integer.parseInt(DynamicSixDigString);
+        return IntSixDigString;
+    }
+
+    public int IntSixDigStringPlusPlus()
+    {
+        IntSixDigString++;
+        return IntSixDigString;
+    }
+
+    public int recordStringLengthAfterIncrease()
+    {
+        StringLengthAfterIncrease =  Integer.parseInt(DynamicSixDigString);
+        return StringLengthAfterIncrease;
+    }
+
+    public Boolean checkHighestPlaceValueChanged()
+    {
+        if (StringLengthBeforeIncrease != StringLengthAfterIncrease)
+        {
+            HighestPlaceValueChanged = true;
+        }
+        else if (StringLengthBeforeIncrease == StringLengthAfterIncrease)
+        {
+            HighestPlaceValueChanged = false;
+        }
+        return HighestPlaceValueChanged;
+    }
+
+}
+
+/*
+
+    if (SixDigString.charAt(0) == 0 && SixDigString.charAt(1) != 0) // 1 (0)00000
         {
             //
             SixDigString.substring(1);
@@ -31,7 +140,7 @@ class SixDigFuncClass
             ZerosToAddBack = "0";
             SixDigString = ZerosToAddBack + SixDigString;
         }
-        else if (SixDigString.charAt(1) == 0) // 2 0(0)0000
+        else if (SixDigString.charAt(1) == 0 && SixDigString.charAt(2) != 0) // 2 0(0)0000
         {
             //
             SixDigString.substring(2);
@@ -41,7 +150,7 @@ class SixDigFuncClass
             ZerosToAddBack = "00";
             SixDigString = ZerosToAddBack + SixDigString;
         }
-        else if (SixDigString.charAt(2) == 0) // 3 00(0)000
+        else if (SixDigString.charAt(2) == 0 && SixDigString.charAt(3) != 0) // 3 00(0)000
         {
             //
             SixDigString.substring(3);
@@ -51,7 +160,7 @@ class SixDigFuncClass
             ZerosToAddBack = "000";
             SixDigString = ZerosToAddBack + SixDigString;
         }
-        else if (SixDigString.charAt(3) == 0) // 4 000(0)00
+        else if (SixDigString.charAt(3) == 0 && SixDigString.charAt(4) != 0) // 4 000(0)00
         {
             //
             SixDigString.substring(4);
@@ -61,7 +170,7 @@ class SixDigFuncClass
             ZerosToAddBack = "0000";
             SixDigString = ZerosToAddBack + SixDigString;
         }            
-        else if (SixDigString.charAt(4) == 0) // 5 0000(0)0
+        else if (SixDigString.charAt(4) == 0 && SixDigString.charAt(5) != 0) // 5 0000(0)0
         {
             //
             SixDigString.substring(5);
@@ -77,7 +186,7 @@ class SixDigFuncClass
             SixDigInt = Integer.parseInt(SixDigString);
             SixDigInt++;
             SixDigString = String.valueOf(SixDigInt);
+            System.out.println(SixDigString);
         }
         return SixDigString;
-    }  
-}
+        */
