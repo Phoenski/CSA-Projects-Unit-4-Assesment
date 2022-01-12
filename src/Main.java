7e649058bfd9f087f9b4008f395f83768627ba68
Phoenski <69543226+Phoenski@users.noreply.github.com>
2022-01-12 16:20:03 -0500

    re

--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,49 @@ import java.io.*;
 
 public class Main
 {
+    private String ResultString;
     public static void main(String [] args) throws IOException
     {
-        String ResultString;
-        int adderTemp = 0;
+        //
+        int TAdderCurrent = 0;
+        int VAdderCurrent = 0;
+        int LAdderCurrent = 0;
+        int XAdderCurrent = 0;
+        int YAdderCurrent = 0;
+        int ZAdderCurrent = 0;
         SixDigFuncClass InitialSixDigString = new SixDigFuncClass("000000");
-        BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt", true));
-        ZLevel adder = new ZLevel(0);
+        //BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt", true));
+        Printer Print = new Printer(0);
         for (int i = -1; i < 999999; i++)
         {
-            InitialSixDigString.toString();
-            InitialSixDigString.PosOfFirstZero();
-            InitialSixDigString.getLeadingZeros();
-            InitialSixDigString.doTruncateLeadingZeros();
-            InitialSixDigString.doIntSixDigStringPlusPlus();
-            InitialSixDigString.doIntToString();
-            InitialSixDigString.doSixDigStringPlusPlus();
-            InitialSixDigString.FinalStringLength();
-            InitialSixDigString.CheckStringLength();
-            ResultString = InitialSixDigString.FinalStringSixDigString();
-            writer.append("/* z = " + adderTemp + " */\n{\n\"" + ResultString + "\"\n},\n");
-            adder.doLevelPlusPlus();
-            adder.doZIntToStringToInt();
-            adder.CheckFor9();
-            adderTemp = adder.SetTo0();
-            if (adderTemp == 9)
+            writer.append("/* z = " + ZAdderCurrent + " */\n{\n\t\"" + ResultString + "\"\n},\n");
+            if (ZAdderCurrent == 9)
             {
                 writer.append("\n");
             }
-            
+            ZAdder.doLevelPlusPlus();
+            ZAdder.doZIntToStringToInt();
+            ZAdder.CheckFor9();
+            ZAdderCurrent = ZAdder.SetTo0();
         }
         writer.close();
+    }
+
+    public String doCoordInc()
+    {
+        //
+        SixDigFuncClass InitialSixDigString = new SixDigFuncClass("000000");
+        InitialSixDigString.toString();
+        InitialSixDigString.PosOfFirstZero();
+        InitialSixDigString.getLeadingZeros();
+        InitialSixDigString.doTruncateLeadingZeros();
+        InitialSixDigString.doIntSixDigStringPlusPlus();
+        InitialSixDigString.doIntToString();
+        InitialSixDigString.doSixDigStringPlusPlus();
+        InitialSixDigString.FinalStringLength();
+        InitialSixDigString.CheckStringLength();
+        ResultString = InitialSixDigString.FinalStringSixDigString();
+        return ResultString;
     }
 }
 
@@ -166,49 +178,151 @@ class SixDigFuncClass
 
 class ZLevel
 {
-    private int DynamicLevelInt = 0;
-    private String DynamicLevelString = null;
-    private boolean Equals9 = false;
-    public ZLevel(int Level)
+    private int ZDynamicLevelInt = 0;
+    private String ZDynamicLevelString = null;
+    private boolean ZEquals9 = false;
+    public ZLevel(int ZLevelInitial)
     {
         //
-        DynamicLevelInt = Level;
+        ZDynamicLevelInt = ZLevelInitial;
     }
     
     public int doLevelPlusPlus()
     {
         //
-        DynamicLevelInt++;
+        ZDynamicLevelInt++;
         
-        return DynamicLevelInt;
+        return ZDynamicLevelInt;
     }
 
     public int doZIntToStringToInt()
     {
-        DynamicLevelString = String.valueOf(DynamicLevelInt);
-        DynamicLevelInt = Integer.parseInt(DynamicLevelString);
-        return DynamicLevelInt;
+        ZDynamicLevelString = String.valueOf(ZDynamicLevelInt);
+        ZDynamicLevelInt = Integer.parseInt(ZDynamicLevelString);
+        return ZDynamicLevelInt;
     }
 
     public boolean CheckFor9()
     {
-        if (DynamicLevelInt > 9)
+        if (ZDynamicLevelInt > 9)
         {
-            Equals9 = true;
+            ZEquals9 = true;
         }
-        else if (DynamicLevelInt < 9)
+        else if (ZDynamicLevelInt < 9)
         {
-            Equals9 = false;
+            ZEquals9 = false;
         }
-        return Equals9;
+        return ZEquals9;
     }
 
     public int SetTo0()
     {
-        if (Equals9 == true)
+        if (ZEquals9 == true)
         {
-            DynamicLevelInt = 0;
+            ZDynamicLevelInt = 0;
         }
-        return DynamicLevelInt;
+        return ZDynamicLevelInt;
     }
 }
+
+class Printer
+{
+    private int TLevel = 0;
+    private int VLevel = 0;
+    private int LLevel = 0;
+    private int XLevel = 0;
+    private int YLevel = 0;
+    private int ZLevel = 0;
+    private String DynamicResultString;
+    private String PrintOutput;
+    BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt", true));
+    
+    public Printer(String ResultString)
+    {
+        //
+        DynamicResultString = ResultString;
+    }
+
+    public String Print()
+    {
+        for (TLevel = 0; TLevel <= 9; TLevel++)
+        {
+            writer.append("/* t = " + TLevel + " */");
+            for (VLevel = 0; VLevel <=9; VLevel++)
+            {
+                writer.append("/* v = " + VLevel + " */");
+                for (LLevel = 0; LLevel <= 9; LLevel++)
+                {
+                    writer.append("/* l = " + LLevel + " */");
+                    for (XLevel = 0; XLevel <= 9; XLevel++)
+                    {
+                        writer.append("/* x = " + XLevel + " */");
+                        for (YLevel = 0; YLevel <= 9; YLevel++)
+                        {
+                            writer.append("/* y = " + YLevel + " */");
+                            for (ZLevel = 0; ZLevel <= 9; ZLevel++)
+                            {
+                                writer.append("/* z = " + ZLevel + " */ \n{\n\t\"" + DynamicResultString + "\"");
+                                if (ZLevel == 9)
+                                {
+                                    writer.append("\n}");
+                                }
+                                else
+                                {
+                                    writer.append("\n},");
+                                }
+                            }
+                            if (YLevel == 9)
+                            {
+                                writer.append("\n}");
+                            }
+                            else
+                            {
+                                writer.append("\n},");
+                            }
+                        }
+                        if (XLevel == 9)
+                        {
+                            writer.append("\n}");
+                        }
+                        else
+                        {
+                            writer.append("\n},");
+                        }
+                    }
+                    if (LLevel == 9)
+                    {
+                        writer.append("\n}");
+                    }
+                    else
+                    {
+                        writer.append("\n},");
+                    }
+                }
+                if (VLevel == 9)
+                {
+                writer.append("\n}");
+                }
+                else
+                {
+                    writer.append("\n},");
+                }
+            }
+            if (TLevel == 9)
+            {
+                writer.append("\n}");
+            }
+            else
+            {
+                writer.append("\n},");
+            }
+        }
+        return PrintOutput;
+    }
+}
+/*
+class YLevel
+{
+    
+}
+*/
